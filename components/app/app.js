b3526c04b6b18fda0c53fdf20161d1df4798f9bf
Charlie Wagner <charlie.wagner@6dglobal.com>
2016-03-10 19:46:58 -0500

    use html5 routing and replace index.html with index2.html

--- components/app/app.js
+++ components/app/app.js
@@ -1,4 +1,7 @@
 var app = angular.module('app', ['ngComponentRouter','app.home', 'app.app-nav', 'app.blog', 'app.projects']);
+app.config(function($locationProvider) {
+  $locationProvider.html5Mode(true);
+})
 app.value('$routerRootComponent', 'app');
 app.component('app', {
   templateUrl: 'components/app/app.html',
